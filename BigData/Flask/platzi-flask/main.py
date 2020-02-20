17812f5113dc75589fb14411aff4eca9975be816
Arnulfo R <drarn96@gmail.com>
2020-02-20 12:18:12 -0500

    App factory y organización

--- BigData/Flask/platzi-flask/main.py
+++ BigData/Flask/platzi-flask/main.py
@@ -1,25 +1,18 @@
-from flask import Flask, request, make_response, redirect, render_template, session, url_for, flash
+from flask import request, make_response, redirect, render_template, session, url_for, flash
 from flask_bootstrap import Bootstrap
-from flask_wtf import FlaskForm
-from wtforms.fields import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+
 import unittest
+
+from app import create_app
+from app.forms import LoginForm
+
 
 import werkzeug
 werkzeug.cached_property = werkzeug.utils.cached_property
 
-app = Flask(__name__)
-app.config['ENV'] = 'development'
-app.config['SECRET_KEY']='SUPER SECRETO'
-bootstrap=Bootstrap(app)
+app = create_app()
 
 todos = ['Comprar café', 'Enviar solicitud de compra', 'Entregar video a producto']
-
-class LoginForm(FlaskForm):
-    username = StringField('Nombre de usuario', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Enviar')
-
 
 @app.cli.command()
 def test():
