5f1c2956d9c64d3740c2778c05153ba826ef710a
Arnulfo R <drarn96@gmail.com>
2020-02-20 10:57:55 -0500

    Pruebas básicas con Flask-testing

--- BigData/Flask/platzi-flask/main.py
+++ BigData/Flask/platzi-flask/main.py
@@ -3,6 +3,10 @@ from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms.fields import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
+import unittest
+
+import werkzeug
+werkzeug.cached_property = werkzeug.utils.cached_property
 
 app = Flask(__name__)
 app.config['ENV'] = 'development'
@@ -15,6 +19,12 @@ class LoginForm(FlaskForm):
     username = StringField('Nombre de usuario', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Enviar')
+
+
+@app.cli.command()
+def test():
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner().run(tests)
 
 @app.errorhandler(404)
 def not_fount(error):
