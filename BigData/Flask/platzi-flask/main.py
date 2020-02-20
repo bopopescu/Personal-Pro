6eedb599062a7f6e838c1a11f5c2f0411f00d2b4
Arnulfo R <drarn96@gmail.com>
2020-02-19 20:35:00 -0500

    Flask-WTF y método Post

--- BigData/Flask/platzi-flask/main.py
+++ BigData/Flask/platzi-flask/main.py
@@ -1,5 +1,8 @@
-from flask import Flask, request, make_response, redirect, render_template, session
+from flask import Flask, request, make_response, redirect, render_template, session, url_for
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 app.config['ENV'] = 'development'
@@ -7,6 +10,11 @@ app.config['SECRET_KEY']='SUPER SECRETO'
 bootstrap=Bootstrap(app)
 
 todos = ['Comprar café', 'Enviar solicitud de compra', 'Entregar video a producto']
+
+class LoginForm(FlaskForm):
+    username = StringField('Nombre de usuario', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Enviar')
 
 @app.errorhandler(404)
 def not_fount(error):
@@ -23,14 +31,25 @@ def index():
     session['user_ip']=user_ip
     return response
 
-@app.route('/hello')
+@app.route('/hello', methods =['GET','POST'])
 def hello():
     #Obtiene la ip del usuario desde la cookie
     user_ip = session.get('user_ip')
+    login_form = LoginForm()
+    username = session.get('username')
+    
     context = {
         'user_ip' : user_ip, 
-        'todos' : todos
+        'todos' : todos,
+        'login_form' : login_form,
+        'username' : username
     }
+
+    if login_form.validate_on_submit():
+        username = login_form.username.data
+        session['username'] = username
+        return redirect(url_for('index'))
+
     return render_template('hello.html', **context)
 
 if __name__ == '__main__':
