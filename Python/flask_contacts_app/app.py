25363f2f704ad0485d5724a2c77a5e14c2699b08
Arnulfo R <drarn96@gmail.com>
2020-02-28 10:31:53 -0500

    Haciendo consultas a la base da datos

--- Python/flask_contacts_app/app.py
+++ Python/flask_contacts_app/app.py
@@ -16,7 +16,10 @@ app.secret_key = 'mysecretkey'
 
 @app.route('/')
 def Index():
-    return render_template('index.html') 
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM contacts')
+    data = cur.fetchall()
+    return render_template('index.html', contacts = data) 
 
 @app.route('/add_contact', methods=['POST'])
 def add_contact():
