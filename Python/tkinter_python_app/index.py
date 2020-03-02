a612d17cf43e0b420564e920290c105c7ef653a1
Arnulfo R <drarn96@gmail.com>
2020-03-02 11:51:34 -0500

    Primer Frame, label y casilla de Input

--- Python/tkinter_python_app/index.py
+++ Python/tkinter_python_app/index.py
@@ -1,14 +1,19 @@
 from tkinter import ttk
 from tkinter import *
 
-import sqlite3
-con = sqlite3.connect('mydatabase.db')
-
 class Product:
     def __init__(self, window):
         self.wind = window
         self.wind.title('Products Application')
 
+        #Creating a Frame Container
+        frame = LabelFrame(self.wind, text = 'Register a new product')
+        frame.grid(row = 0, column = 0, columnspan = 3, pady = 20)
+
+        #Name input
+        Label(frame, text = 'Name: ').grid(row = 1, column = 0)
+        self.name = Entry(frame)
+        self.name.grid(row = 1, column = 1)
 
 
 if __name__ == '__main__':
