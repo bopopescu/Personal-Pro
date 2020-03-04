160bc1e5450bb151458fc24dac7a754ca9b9d558
Arnulfo R <drarn96@gmail.com>
2020-03-03 21:24:29 -0500

    Touch Input / Mouse Input

--- Python/kivy_app/main.py
+++ Python/kivy_app/main.py
@@ -1,13 +1,27 @@
 import kivy
 from kivy.app import App
-from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
 
 
+class Touch(Widget):
+    btn = ObjectProperty(None)
+    
+    def on_touch_down(self, touch):
+        super().on_touch_down(touch)
+        print("Mouse down", touch)
+
+    def on_touch_move(self, touch):
+        print("Mouse Move", touch)
+
+    def on_touch_up(self, touch):
+        print("Mouse up", touch)
+        self.btn.opacity = 1
 
 
 class MyApp(App):
     def build(self):
-        return FloatLayout()
+        return Touch()
 
 
 if __name__ == '__main__':
