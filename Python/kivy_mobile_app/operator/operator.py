b3e9fa2083c8efaf9c1156910de45e9daf11e401
Arnulfo R <drarn96@gmail.com>
2020-03-05 16:19:21 -0500

    Creación del preview

--- Python/kivy_mobile_app/operator/operator.py
+++ Python/kivy_mobile_app/operator/operator.py
@@ -9,7 +9,7 @@ class OperatorWindow(BoxLayout):
     def update_purchases(self):
         code = self.ids.code_inp.text
         products_container = self.ids.products
-        
+
         if code == '1234':
             details = BoxLayout(size_hint_y = None, height = 30, pos_hint = {'top':1})
             products_container.add_widget(details)
@@ -27,6 +27,17 @@ class OperatorWindow(BoxLayout):
             details.add_widget(price)
             details.add_widget(total)
 
+            #Update Preview
+            pname = "Product One"
+            pprice = 1.00
+            preview = self.ids.receipt_preview
+            prev_text = preview.text
+            _prev = prev_text.find('`')
+            if _prev > 0:
+                prev_text = prev_text[:_prev]
+            purchase_total = '`\n\nTotal\t\t\t\t\t\t\t\t0.00'
+            nu_preview = '\n'.join([prev_text,pname+'\t\t\t\t'+str(pprice), purchase_total ])
+            preview.text = nu_preview
 
 
 class OperatorApp(App):
