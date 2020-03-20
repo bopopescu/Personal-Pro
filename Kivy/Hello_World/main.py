8a13258a6b063168d8a0ffce4bc4eefdaa90e082
Arnulfo R <drarn96@gmail.com>
2020-03-19 23:32:11 -0500

    Hello World con tres diferentes formas de generar un Label

--- Kivy/Hello_World/main.py
+++ Kivy/Hello_World/main.py
@@ -5,7 +5,19 @@ from kivy.uix.label import Label
 
 
 def build():
-    return Label(text="Hello World")
+    # Form One
+    # lb = Label(text = "Curso de Python e Kivy", italic = True, font_size = 50)
+
+    # Form Two
+    # lb = Label()
+    # lb.text = "Curso de Python e Kivy"
+    # lb.italic = True
+    # lb.font_size = 50
+
+    # Form Three
+    formato = {'text': "Curso de Python e Kivy", 'italic': True, 'font_size': 50}
+    lb = Label(**formato)
+    return lb
 
 
 hello_world = App()
