298d956667fb8dc35128c71a6df5ef5cedfe4127
Arnulfo R <drarn96@gmail.com>
2020-03-20 12:48:53 -0500

    Hello World con text input, botón y Label con programación orientada a objetos

--- Kivy/Hello_World/main.py
+++ Kivy/Hello_World/main.py
@@ -8,44 +8,45 @@ from kivy.core.window import Window
 from kivy.uix.label import Label
 
 
-def click():
-    print('Hi')
+class MyProgram(App):
+
+    @staticmethod
+    def click():
+        print('Hi')
+
+    def build(self):
+        layout = FloatLayout()
+
+        ed = TextInput(text="Hello World")
+        ed.size_hint = None, None
+        ed.height = 300
+        ed.width = 400
+        ed.x = 100
+        ed.y = 250
+
+        bt = Button(text="Click")
+        bt.size_hint = None, None
+        bt.width = 200
+        bt.height = 50
+        bt.y = 150
+        bt.x = 200
+        bt.on_press = MyProgram.click
+
+        formato = {'text': "Hello World With Label, Text Input and Button", 'italic': True, 'font_size': 30}
+        lb = Label(**formato)
+        lb.size_hint = None, None
+        lb.width = 400
+        lb.height = 50
+        lb.y = 50
+        lb.x = 100
+
+        layout.add_widget(ed)
+        layout.add_widget(bt)
+        layout.add_widget(lb)
+        return layout
 
 
-def build():
-    layout = FloatLayout()
-
-    ed = TextInput(text="Hello World")
-    ed.size_hint = None, None
-    ed.height = 300
-    ed.width = 400
-    ed.x = 100
-    ed.y = 250
-
-    bt = Button(text="Click")
-    bt.size_hint = None, None
-    bt.width = 200
-    bt.height = 50
-    bt.y = 150
-    bt.x = 200
-    bt.on_press = click
-
-    formato = {'text': "Hello World With Label, Text Input and Button", 'italic': True, 'font_size': 30}
-    lb = Label(**formato)
-    lb.size_hint = None, None
-    lb.width = 400
-    lb.height = 50
-    lb.y = 50
-    lb.x = 100
-
-    layout.add_widget(ed)
-    layout.add_widget(bt)
-    layout.add_widget(lb)
-    return layout
-
-
-window = App()
+window = MyProgram()
 window.title = "Hello"
 Window.size = 600, 600
-window.build = build
 window.run()
