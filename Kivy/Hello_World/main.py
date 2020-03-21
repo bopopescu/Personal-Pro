57f95d1a46ad50de1a70a3bf1b67ad67dae41de1
Arnulfo R <drarn96@gmail.com>
2020-03-21 09:46:50 -0500

    Hello World con 2 Screens y BoxLayout

--- Kivy/Hello_World/main.py
+++ Kivy/Hello_World/main.py
@@ -6,16 +6,13 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.core.window import Window
 from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
 
 
-class MyProgram(App):
+class FirstWindow(FloatLayout):
 
-    @staticmethod
-    def click():
-        print('Hi')
-
-    def build(self):
-        layout = FloatLayout()
+    def __init__(self, **kwargs):
+        super(FirstWindow, self).__init__(**kwargs)
 
         ed = TextInput(text="Hello World")
         ed.size_hint = None, None
@@ -30,7 +27,7 @@ class MyProgram(App):
         bt.height = 50
         bt.y = 150
         bt.x = 200
-        bt.on_press = MyProgram.click
+        bt.on_press = self.on_press_bt
 
         formato = {'text': "Hello World With Label, Text Input and Button", 'italic': True, 'font_size': 30}
         lb = Label(**formato)
@@ -40,10 +37,33 @@ class MyProgram(App):
         lb.y = 50
         lb.x = 100
 
-        layout.add_widget(ed)
-        layout.add_widget(bt)
-        layout.add_widget(lb)
-        return layout
+        self.add_widget(ed)
+        self.add_widget(bt)
+        self.add_widget(lb)
+
+    def on_press_bt(self):
+        window.root_window.remove_widget(window.root)
+        window.root_window.add_widget(SecondWindow())
+
+
+class SecondWindow(BoxLayout):
+
+    def __init__(self, **kwargs):
+        super(SecondWindow, self).__init__(**kwargs)
+        self.orientation = "vertical"
+        lb = Label(text= "Hello World Label")
+        bt = Button(text="Click Here")
+        bt.on_press = self.on_press_bt
+        self.add_widget(lb)
+        self.add_widget(bt)
+
+    def on_press_bt(self):
+        print("Hello")
+
+
+class MyProgram(App):
+    def build(self):
+        return FirstWindow()
 
 
 window = MyProgram()
